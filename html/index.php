cd82d4edbafa190f71ec836670241d7158610e8d
Florian P <palabostf@gmail.com>
2021-03-04 16:11:51 +0100

    [setup-NPMP] setup with mysql & phpmyadmin
    NPMP : nginx, php8, mysql, phpmyadmin

--- html/index.php
+++ html/index.php
@@ -1,7 +1,7 @@
 <html>
     <body>
         <?php require_once('layout/header.php') ?>
-        <h1>HEloo world</h1>
+        <h1>hello world</h1>
         <?php require_once('sandbox/listProducts.php') ?>
         <?php require_once('sandbox/listClients.php') ?>
         <?php require_once('layout/footer.php') ?>
